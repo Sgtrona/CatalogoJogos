68df61c89d2e8d0156e81c34f358168b00189ad9
Sgtrona <43658661+Sgtrona@users.noreply.github.com>
2021-10-15 20:57:17 -0300

    Add files via upload

---
+++ CatalogoJogos/Entities/Jogo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CatalogoJogos.Entities
+{
+    public class Jogo
+    {
+        public Guid Id { get; set; }
+        public string Nome { get; set; }
+        public string Produtora { get; set; }
+        public double Preco { get; set; }
+    }
+}
---
+++ CatalogoJogos/Exceptions/JogoJaCadastradoException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace CatalogoJogos.Exceptions
+{
+    public class JogoJaCadastradoException : Exception
+    {
+        public JogoJaCadastradoException()
+            : base("Este já jogo está cadastrado")
+        { }
+    }
+}
